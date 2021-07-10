5a42c361b28de0a1e1e3523ea06c740dbc5d304a
Miracle@pro <luomengfanchen@outlook.com>
2021-07-10 09:47:28 +0800

    feat: markdown解析器

---
+++ src/plugins/markdown.js
@@ -0,0 +1,46 @@
+// markdown解析函数
+const markdownParse = function(content) {
+    // 将文章以\n分割为数组
+    let arr = content.split('\n')
+    // 解析后的字符串
+    let str = new String()
+
+    for(let i = 0; i < arr.length; i++) {
+        // 解析标题: 开头为##
+        if(arr[i].indexOf('##') == 0) {
+            str += '<h3 class="content-title2">' + arr[i].slice(3) + '</h3>';
+
+        // 解析代码: 开头为```
+        } else if(arr[i].indexOf('```') == 0) {
+            let codeStr = '<code class="content-code">'
+
+            for(let index = i + 1; index < arr.length; index++) {
+                // 若不为结尾的: ```
+                if (arr[index].indexOf('```') != 0) {
+                    // 对于每一行填充&nbsp;和<br>
+                    let line = arr[index]
+                    line = line.replace(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;')
+                    line = line.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;')
+                    codeStr += line + '<br />'
+
+                // 寻找结尾符号
+                } else {
+                    // 更改索引位置，退出循环
+                    i = index
+                    break
+                }
+            }
+
+            codeStr += '</code>'
+            str += codeStr
+
+        // 解析段落文本
+        } else {
+            str += '<p class="content-paragraph">' + arr[i] + '</p>'
+        }
+    }
+
+    return str
+}
+
+export default markdownParse
