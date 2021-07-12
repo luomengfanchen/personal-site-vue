0e5de6efcd16cc84376e4930c6f01d71bfb54b45
Miracle@pro <luomengfanchen@outlook.com>
2021-07-12 08:19:40 +0800

    feat: 添加了搜索功能

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,34 +4,38 @@
             type="text"
             class="search-bar"
             placeholder="输入关键字搜索文章······"
+            v-model="searchVal"
+            @keypress.enter="search"
         />
 
-        <!-- search result -->
-        <div class="search-result">
-            <div class="result-card">
-                <a class="result-link" href="#">
-                    <h2 class="result-title">什么是HTML?</h2>
-                </a>
-
-                <div class="result-descript">
-                    <span>阅读量：100</span>
-                    <span>发布时间：2020-10-10</span>
-                </div>
-                <p class="result-paragraph">
-                    HTML 是用来描述网页的一种语言。 HTML 指的是超文本标记语言:
-                    HyperText Markup Language HTML
-                    不是一种编程语言，而是一种标记语言 标记语言是一套标记标签
-                    (markup tag) HTML 使用标记标签来描述网页 HTML 文档包含了HTML
-                    标签及文本内容 HTML文档也叫做 web 页面
-                </p>
-            </div>
-        </div>
+        <ContentCard :articleList="articleList"></ContentCard>
     </div>
 </template>
 
 <script>
+import ContentCard from '../components/ContentCard.vue'
+
 export default {
-    name: 'Search'
+    name: 'Search',
+    components: { ContentCard },
+    data() {
+        return {
+            searchVal: '',
+            articleList: []
+        }
+    },
+    methods: {
+        search: function() {
+            this.axios
+                .get('/api/search?title=' + this.searchVal)
+                .then((response) => {
+                    this.articleList = response.data.data
+                })
+                .catch(() => {
+                    window.alert('数据获取失败')
+                })
+        }
+    }
 }
 </script>
 
@@ -53,64 +57,10 @@ export default {
     margin: 10px;
     outline: none;
     padding: 10px 30px;
-    width: 500px;
+    width: 700px;
 }
 .search-bar:focus {
     border: #4169e1 solid 1px;
     transition: all 0.2s ease 0s;
-}
-
-/* 内容容器 */
-.search-result {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-/* 内容 */
-.result-card {
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0px 1px 10px 0px #d3d3d3;
-    display: flex;
-    flex-direction: column;
-    margin-top: 50px;
-    padding: 50px;
-    width: 700px;
-}
-.result-card:hover {
-    box-shadow: 0px 1px 20px 0px #d3d3d3;
-    transition: all 0.2s ease-out 0s;
-}
-
-/* 内容标题 */
-.result-title {
-    border-bottom: #000 solid 2px;
-    color: #000;
-    font-family: 'Source Han Serif';
-    font-weight: bold;
-    margin-bottom: 20px;
-}
-.result-title:hover {
-    border-bottom: #4169e1 solid 2px;
-    color: #4169e1;
-    transition: all 0.2s ease-out 0s;
-}
-.result-link {
-    text-decoration: none;
-}
-
-.result-descript {
-    align-self: center;
-    color: #555;
-    font-family: 'Source Han Serif';
-    margin-bottom: 20px;
-}
-
-/* 内容段落 */
-.result-paragraph {
-    color: #555;
-    font-family: 'Source Han Serif';
-    text-indent: 2em;
 }
 </style>
